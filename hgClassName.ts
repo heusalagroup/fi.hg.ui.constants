3a41470d39ebe35c10294f2869305280da4389b2
Jaakko Heusala <jhh@heusalagroup.fi>
2022-03-03 21:05:40 +0200

    Added constants

---
+++ hgClassName.ts
@@ -0,0 +1,8 @@
+
+export const SCROLL_TO_HERE_CLASS_NAME = 'scroll-to-here';
+export const BACK_LINK_CLASS_NAME      = 'back-link';
+export const BACK_BUTTON_CLASS_NAME    = 'back-link';
+export const BUTTON_CLASS_NAME = 'button';
+export const TIMER_CLASS_NAME = 'timer';
+export const DISABLED_BUTTON_CLASS_NAME = 'button-disabled';
+export const SUBMIT_BUTTON_CLASS_NAME = 'submit-button';
